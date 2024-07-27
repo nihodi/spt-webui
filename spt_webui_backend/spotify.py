3a193f802510f767593da7e00d2f5d2e6a5f7dc6
Niklas Dietzel <niklas@dietzel.no>
2024-07-27 19:33:30 +0200

    refactor _do_get_request to generic _do_request

--- spt_webui_backend/spotify.py
+++ spt_webui_backend/spotify.py
@@ -1,6 +1,6 @@
 import datetime
 import urllib.parse
-from typing import Dict, Tuple, Optional
+from typing import Dict, Tuple, Optional, Literal
 
 import requests
 from requests_oauthlib import OAuth2Session
@@ -28,7 +28,7 @@ def add_track_to_queue(
 
 
 class Spotify:
-    _get_cache: Dict[str, Tuple[requests.Response, datetime.datetime]] = {}
+    _cache: Dict[str, Tuple[requests.Response, datetime.datetime]] = {}
 
     def __init__(self, session: Optional[OAuth2Session] = None):
         if not session:
@@ -37,21 +37,22 @@ class Spotify:
             self.session = session
 
     def get_playback_state(self):
-        resp = self._do_get_request("https://api.spotify.com/v1/me/player")
+        resp = self._do_request("GET", "https://api.spotify.com/v1/me/player")
         if resp.status_code == 204:
             return None
 
         return resp.json()
 
     def get_me(self) -> schemas.SpotifyUser:
-        return schemas.SpotifyUser.model_validate(self._do_get_request("https://api.spotify.com/v1/me").json())
+        return schemas.SpotifyUser.model_validate(self._do_request("GET", "https://api.spotify.com/v1/me").json())
 
     def get_playback_queue(self):
-        return self._do_get_request("https://api.spotify.com/v1/me/player/queue").json()
+        return self._do_request("GET", "https://api.spotify.com/v1/me/player/queue").json()
 
-    def _do_get_request(self, url: str, can_cache: bool = True, **kwargs) -> requests.Response:
+    def _do_request(self, method: Literal["GET", "POST"], url: str, can_cache: bool = True,
+                    **kwargs) -> requests.Response:
         if can_cache:
-            cache = self._get_cache.get(url)
+            cache = self._cache.get(f"{method} {url}")
 
             if cache is not None:
                 (resp, timestamp) = cache
@@ -60,17 +61,25 @@ class Spotify:
                 if (datetime.datetime.now() - timestamp).total_seconds() < 15:
                     return resp
 
-        resp = self.session.get(url, **kwargs)
+        match method:
+            case "GET":
+                fn = self.session.get
+            case "POST":
+                fn = self.session.post
+            case _:
+                raise ValueError("Unsupported method")
+
+        resp = fn(url, **kwargs)
 
         # retry once if access token is invalid
         if resp.status_code == 401:
             self._refresh_token()
-            resp = self.session.get(url, **kwargs)
+            resp = fn(url, **kwargs)
 
         resp.raise_for_status()
 
         if can_cache:
-            self._get_cache[url] = (resp, datetime.datetime.now())
+            self._cache[f"{method} {url}"] = (resp, datetime.datetime.now())
 
         return resp
 
