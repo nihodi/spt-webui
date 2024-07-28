1c3951a233b5d6896e376dfa098d953fbe753bb8
Niklas Dietzel <niklas@dietzel.no>
2024-07-28 19:32:19 +0200

    formatting

--- spt-webui-frontend/src/app/track-list/track-list.component.ts
+++ spt-webui-frontend/src/app/track-list/track-list.component.ts
@@ -2,11 +2,11 @@ import { Component, input } from '@angular/core';
 import { TrackObject } from "../api-services/models";
 
 @Component({
-  selector: 'app-track-list',
-  standalone: true,
-  imports: [],
-  templateUrl: './track-list.component.html',
-  styleUrl: './track-list.component.scss'
+	selector: 'app-track-list',
+	standalone: true,
+	imports: [],
+	templateUrl: './track-list.component.html',
+	styleUrl: './track-list.component.scss'
 })
 export class TrackListComponent {
 	tracks = input.required<TrackObject[]>();
