86e4426ba8ddf44cb28c32c0cd74cd72080ccca3
Niklas Dietzel <niklas@dietzel.no>
2024-07-29 15:36:37 +0200

    add authInterceptor

--- spt-webui-frontend/src/app/app.routes.ts
+++ spt-webui-frontend/src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from "./app.component";
+import { IndexComponent } from "./pages/index/index.component";
 
 export const routes: Routes = [
+	{
+		path: "",
+		component: IndexComponent,
+	}
 ];
