3027d76132884deb0465fdcb2f70d11356dc3e25
Niklas Dietzel <niklas@dietzel.no>
2024-07-29 15:37:02 +0200

    prepare for login page
    moves everything currently in AppComponent to a new IndexComponent

--- spt-webui-frontend/src/app/app.component.ts
+++ spt-webui-frontend/src/app/app.component.ts
@@ -1,71 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AsyncPipe, JsonPipe } from "@angular/common";
-import {
-	AbstractControl,
-	FormBuilder,
-	FormControl,
-	FormGroup,
-	ReactiveFormsModule, ValidationErrors,
-	ValidatorFn,
-	Validators
-} from "@angular/forms";
-import { SptWebUiApiWrapperService } from "./api-services/spt-web-ui-api-wrapper.service";
-import { PlaybackStateService } from "./state-services/playback-state.service";
-import { TrackCardComponent } from "./track-card/track-card.component";
-import { TrackListComponent } from "./track-list/track-list.component";
-import { HttpErrorResponse } from "@angular/common/http";
-
-const matchesSpotifyUrl: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
-	const regex = /https:\/\/open.spotify.com\/track\/[a-zA-Z0-9]+/g;
-
-	if (regex.test(control.value))
-		return null;
-
-	return {
-		"spotifyUrl": "Does not match a valid spotify URL!"
-	};
-}
 
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [RouterOutlet, JsonPipe, ReactiveFormsModule, AsyncPipe, TrackCardComponent, TrackListComponent],
+	imports: [RouterOutlet],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.scss'
 })
 export class AppComponent {
 
-	constructor(
-		private fb: FormBuilder,
-		private apiWrapper: SptWebUiApiWrapperService,
-		protected playbackState: PlaybackStateService
-		) {
-		this.addToQueueForm = this.fb.group({
-			url: ['', [Validators.required, matchesSpotifyUrl]]
-		});
-	}
 
-	addToQueueForm: FormGroup<{ url: FormControl<string | null> }>;
-
-	addToQueue($event: SubmitEvent) {
-		const url = new URL(this.addToQueueForm.value.url!);
-		url.search = "";
-
-		this.addToQueueForm.disable();
-
-		this.apiWrapper.addSongToQueue(url.toString()).subscribe({
-			next: () => {
-				setTimeout(() => {
-					this.playbackState.updatePlaybackState();
-				}, 10);
-
-				this.addToQueueForm.reset();
-				this.addToQueueForm.enable();
-			}, error: (err: HttpErrorResponse) => {
-				this.addToQueueForm.enable();
-			}
-		})
-	}
 }
---
+++ spt-webui-frontend/src/app/pages/index/index.component.ts
@@ -0,0 +1,74 @@
+import { Component } from '@angular/core';
+import {
+	AbstractControl,
+	FormBuilder,
+	FormControl,
+	FormGroup, ReactiveFormsModule,
+	ValidationErrors,
+	ValidatorFn,
+	Validators
+} from "@angular/forms";
+import { SptWebUiApiWrapperService } from "../../api-services/spt-web-ui-api-wrapper.service";
+import { PlaybackStateService } from "../../state-services/playback-state.service";
+import { HttpErrorResponse } from "@angular/common/http";
+import { TrackCardComponent } from "../../track-card/track-card.component";
+import { AsyncPipe } from "@angular/common";
+import { TrackListComponent } from "../../track-list/track-list.component";
+
+const matchesSpotifyUrl: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+	const regex = /https:\/\/open.spotify.com\/track\/[a-zA-Z0-9]+/g;
+
+	if (regex.test(control.value))
+		return null;
+
+	return {
+		"spotifyUrl": "Does not match a valid spotify URL!"
+	};
+}
+
+
+@Component({
+  selector: 'app-index',
+  standalone: true,
+	imports: [
+		TrackCardComponent,
+		AsyncPipe,
+		ReactiveFormsModule,
+		TrackListComponent
+	],
+  templateUrl: './index.component.html',
+  styleUrl: './index.component.scss'
+})
+export class IndexComponent {
+	constructor(
+		private fb: FormBuilder,
+		private apiWrapper: SptWebUiApiWrapperService,
+		protected playbackState: PlaybackStateService
+	) {
+		this.addToQueueForm = this.fb.group({
+			url: ['', [Validators.required, matchesSpotifyUrl]]
+		});
+	}
+
+	addToQueueForm: FormGroup<{ url: FormControl<string | null> }>;
+
+	addToQueue($event: SubmitEvent) {
+		const url = new URL(this.addToQueueForm.value.url!);
+		url.search = "";
+
+		this.addToQueueForm.disable();
+
+		this.apiWrapper.addSongToQueue(url.toString()).subscribe({
+			next: () => {
+				setTimeout(() => {
+					this.playbackState.updatePlaybackState();
+				}, 10);
+
+				this.addToQueueForm.reset();
+				this.addToQueueForm.enable();
+			}, error: (err: HttpErrorResponse) => {
+				this.addToQueueForm.enable();
+			}
+		});
+	}
+}
