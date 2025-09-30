413da1e67516980c87e38ce9b9ae37ef90b4d527
Niklas Dietzel <niklas@dietzel.no>
2025-09-30 20:09:28 +0200

    nix: attempt fix when baseHref is null

--- nix/module.nix
+++ nix/module.nix
@@ -25,6 +25,35 @@ in
         type = lib.types.path;
         description = "File to load as environment. Used to configure secret options.";
       };
+
+      baseHref = {
+        type = lib.types.string;
+        description = "Base path where the application is deployed";
+        example = "https://dodooc.dietzel.no/";
+      };
+
+      spotify = {
+        allowedAccountId = {
+          type = lib.types.string;
+          description = "Account ID of the Spotify account that should be controlled from spt-webui";
+        };
+
+        clientId = {
+          type = lib.types.string;
+          description = "Spotify OAuth2 client id. The client secret is set using the `environmentFile`.";
+        };
+
+        playlistId = {
+          type = lib.types.nullOr lib.types.string;
+          default = null;
+          description = "Spotify playlist all requested songs get added to.";
+        };
+      };
+
+      discordClientId = {
+        type = lib.types.string;
+        description = "Discord OAuth2 client id. The client secret is set using the `environmentFile`";
+      };
     };
   };
 
@@ -50,6 +79,27 @@ in
         }/bin/spt_webui_backend --env-file ${cfg.settings.environmentFile} --env-file /etc/spt-webui/env";
       };
       wantedBy = [ "multi-user.target" ];
+    };
+
+    environment.etc."spt-webui/env" = {
+      text = ''
+        FRONTEND_URL=${cfg.settings.baseHref}
+        ALLOWED_ORIGIN=${cfg.settings.baseHref}:80
+        API_PREFIX=/api
+
+        DISCORD_REDIRECT_URI=${cfg.settings.baseHref}api/auth/callback/discord
+
+        SPOTIFY_REDIRECT_URI=${cfg.settings.baseHref}api/auth/callback
+        SPOTIFY_CLIENT_ID=${cfg.settings.spotifyClientId}
+        SPOTIFY_ALLOWED_ACCOUNT_ID=${cfg.settings.spotifyAllowedAccountId}
+
+        TOKEN_SAVE_LOCATION=/var/spt-webui/saved_token
+
+        ${lib.optionalString cfg.settings.spotify.playlistId != null cfg.settings.spotify.playlistId}
+      '';
+      user = "spt-webui-backend";
+      group = "spt-webui-backend";
+      mode = "0440";
     };
 
     systemd.tmpfiles.rules = [
