d98099cff17565f5954d714337f7a2649f3828cf
Niklas Dietzel <niklas@dietzel.no>
2025-09-30 20:45:51 +0200

    nix: add nginx integration

--- nix/module.nix
+++ nix/module.nix
@@ -20,6 +20,10 @@ in
       enable = lib.mkEnableOption "MySQL/MariaDB account and database creation";
     };
 
+    nginx = {
+      enable = lib.mkEnableOption "nginx integration";
+    };
+
     settings = {
 
       # TODO: declare settings other than this
@@ -30,10 +34,16 @@ in
         description = "File to load as environment. Used to configure secret options.";
       };
 
+      domain = lib.mkOption {
+        type = lib.types.str;
+        description = "Domain where the application is hosted";
+      };
+
       baseHref = lib.mkOption {
         type = lib.types.string;
         description = "Base path where the application is deployed";
-        example = "https://dodooc.dietzel.no/";
+        example = "/spt-webui";
+        default = "/";
       };
 
       spotify = {
@@ -85,32 +95,34 @@ in
       wantedBy = [ "multi-user.target" ];
     };
 
-    environment.etc."spt-webui/env" = {
-      text = ''
-        FRONTEND_URL=${cfg.settings.baseHref}
-        ALLOWED_ORIGIN=${cfg.settings.baseHref}:80
-        API_PREFIX=/api
+    environment.etc."spt-webui/env" =
+      let
+        base = "https://${cfg.settings.domain}${cfg.settings.baseHref}";
+      in
+      {
+        text = ''
+          FRONTEND_URL=${base}
+          ALLOWED_ORIGIN=${base}:80
+          API_PREFIX=/api
 
-        DISCORD_REDIRECT_URI=${cfg.settings.baseHref}api/auth/callback/discord
-        DISCORD_CLIENT_ID=${cfg.settings.discordClientId}api/auth/callback/discord
+          DISCORD_REDIRECT_URI=${base}/api/auth/callback/discord
+          DISCORD_CLIENT_ID=${cfg.settings.discordClientId}
 
-        SPOTIFY_REDIRECT_URI=${cfg.settings.baseHref}api/auth/callback
-        SPOTIFY_CLIENT_ID=${cfg.settings.spotify.clientId}
-        SPOTIFY_ALLOWED_ACCOUNT_ID=${cfg.settings.spotify.allowedAccountId}
-        TOKEN_SAVE_LOCATION=/var/spt-webui/saved_token
+          SPOTIFY_REDIRECT_URI=${base}/api/auth/callback
+          SPOTIFY_CLIENT_ID=${cfg.settings.spotify.clientId}
+          SPOTIFY_ALLOWED_ACCOUNT_ID=${cfg.settings.spotify.allowedAccountId}
+          TOKEN_SAVE_LOCATION=/var/spt-webui/saved_token
 
-        ${lib.optionalString (
-          cfg.settings.spotify.playlistId != null
-        ) "SPOTIFY_PLAYLIST_ID=${cfg.settings.spotify.playlistId}"}
+          ${lib.optionalString (
+            cfg.settings.spotify.playlistId != null
+          ) "SPOTIFY_PLAYLIST_ID=${cfg.settings.spotify.playlistId}"}
 
-        ${lib.optionalString (
-          cfg.mysql.enable
-        ) "DATABASE_URL=mariadb+pymysql:///spt_webui?unix_socket=/run/mysqld/mysqld.sock"}
-      '';
-      user = "spt-webui-backend";
-      group = "spt-webui-backend";
-      mode = "0440";
-    };
+          ${lib.optionalString (cfg.mysql.enable) "DATABASE_URL=mariadb+pymysql:///spt_webui?unix_socket=/run/mysqld/mysqld.sock"}
+        '';
+        user = "spt-webui-backend";
+        group = "spt-webui-backend";
+        mode = "0440";
+      };
 
     systemd.tmpfiles.rules = [
       "f /var/spt-webui/saved_token 0770 spt-webui-backend spt-webui-backend -"
@@ -124,6 +136,7 @@ in
     users.groups.spt-webui-backend = {
     };
 
+    ## mariadb/mysql integration
     # create database and database user if it is enabled
     services.mysql = lib.mkIf cfg.mysql.enable {
       ensureUsers = [
@@ -137,5 +150,30 @@ in
 
       ensureDatabases = [ "spt_webui" ];
     };
+
+    ## nginx integration
+
+    services.nginx =
+      let
+        spt-packages = inputs.self.packages.${pkgs.system};
+
+        frontend-env = spt-packages.frontend-env {
+          apiPrefix = "https://${cfg.settings.domain}${cfg.settings.baseHref}/api";
+        };
+        frontend = spt-packages.frontend { env = frontend-env; };
+      in
+
+      {
+        virtualHosts.${cfg.settings.domain} = {
+          locations."/${cfg.settings.baseHref}" = {
+            root = "${frontend}";
+            tryFiles = "\$uri /index.html";
+          };
+
+          locations."/${cfg.settings.baseHref}/api" = {
+            proxyPass = "http://localhost:8000";
+          };
+        };
+      };
   };
 }
