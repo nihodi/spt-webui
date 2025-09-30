596f0e3863528548a9bda905e10e93234a8557c2
Niklas Dietzel <niklas@dietzel.no>
2025-09-30 20:46:48 +0200

    nix: move nginx configuration into the correct place

--- nix/module.nix
+++ nix/module.nix
@@ -96,32 +96,33 @@ in
     };
 
     environment.etc."spt-webui/env" =
-      let base = "https://${cfg.settings.domain}${cfg.settings.baseHref}";
+      let
+        base = "https://${cfg.settings.domain}${cfg.settings.baseHref}";
       in
-     {
-      text = ''
-        FRONTEND_URL=${base}
-        ALLOWED_ORIGIN=${base}:80
-        API_PREFIX=/api
+      {
+        text = ''
+          FRONTEND_URL=${base}
+          ALLOWED_ORIGIN=${base}:80
+          API_PREFIX=/api
 
-        DISCORD_REDIRECT_URI=${base}/api/auth/callback/discord
-        DISCORD_CLIENT_ID=${cfg.settings.discordClientId}
+          DISCORD_REDIRECT_URI=${base}/api/auth/callback/discord
+          DISCORD_CLIENT_ID=${cfg.settings.discordClientId}
 
-        SPOTIFY_REDIRECT_URI=${base}/api/auth/callback
-        SPOTIFY_CLIENT_ID=${cfg.settings.spotify.clientId}
-        SPOTIFY_ALLOWED_ACCOUNT_ID=${cfg.settings.spotify.allowedAccountId}
-        TOKEN_SAVE_LOCATION=/var/spt-webui/saved_token
+          SPOTIFY_REDIRECT_URI=${base}/api/auth/callback
+          SPOTIFY_CLIENT_ID=${cfg.settings.spotify.clientId}
+          SPOTIFY_ALLOWED_ACCOUNT_ID=${cfg.settings.spotify.allowedAccountId}
+          TOKEN_SAVE_LOCATION=/var/spt-webui/saved_token
 
-        ${lib.optionalString (
-          cfg.settings.spotify.playlistId != null
-        ) "SPOTIFY_PLAYLIST_ID=${cfg.settings.spotify.playlistId}"}
+          ${lib.optionalString (
+            cfg.settings.spotify.playlistId != null
+          ) "SPOTIFY_PLAYLIST_ID=${cfg.settings.spotify.playlistId}"}
 
-        ${lib.optionalString (cfg.mysql.enable) "DATABASE_URL=mariadb+pymysql:///spt_webui?unix_socket=/run/mysqld/mysqld.sock"}
-      '';
-      user = "spt-webui-backend";
-      group = "spt-webui-backend";
-      mode = "0440";
-    };
+          ${lib.optionalString (cfg.mysql.enable) "DATABASE_URL=mariadb+pymysql:///spt_webui?unix_socket=/run/mysqld/mysqld.sock"}
+        '';
+        user = "spt-webui-backend";
+        group = "spt-webui-backend";
+        mode = "0440";
+      };
 
     systemd.tmpfiles.rules = [
       "f /var/spt-webui/saved_token 0770 spt-webui-backend spt-webui-backend -"
@@ -149,28 +150,30 @@ in
 
       ensureDatabases = [ "spt_webui" ];
     };
-  };
 
-  ## nginx integration
+    ## nginx integration
 
-  services.nginx =
-    let
-      spt-packages = inputs.self.packages.${pkgs.system};
+    services.nginx =
+      let
+        spt-packages = inputs.self.packages.${pkgs.system};
 
-      frontend-env = spt-packages.frontend-env { apiPrefix = "https://${cfg.settings.domain}${cfg.settings.baseHref}/api"; };
-      frontend = spt-packages.frontend { env = frontend-env; };
-    in
-
-    {
-      virtualHosts.${cfg.settings.domain} = {
-        locations."/${cfg.settings.baseHref}" = {
-          root = "${frontend}";
-          tryFiles = "\$uri /index.html";
+        frontend-env = spt-packages.frontend-env {
+          apiPrefix = "https://${cfg.settings.domain}${cfg.settings.baseHref}/api";
         };
+        frontend = spt-packages.frontend { env = frontend-env; };
+      in
 
-        locations."/${cfg.settings.baseHref}/api" = {
-          proxyPass = "http://localhost:8000";
+      {
+        virtualHosts.${cfg.settings.domain} = {
+          locations."/${cfg.settings.baseHref}" = {
+            root = "${frontend}";
+            tryFiles = "\$uri /index.html";
+          };
+
+          locations."/${cfg.settings.baseHref}/api" = {
+            proxyPass = "http://localhost:8000";
+          };
         };
       };
-    };
+  };
 }
