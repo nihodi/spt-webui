ee1f0b3f71286b9809c7ba112400603a4e481ac5
Niklas Dietzel <niklas@dietzel.no>
2025-11-07 18:13:15 +0100

    update `deploy.sh` to make it work again after migrating from poetry to uv
    this is probably going to get removed soon, due to the easier deployment using Nix/NixOS

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@
 set -xeo pipefail
 
 cd spt-webui-frontend
-ng build
+./node_modules/@angular/cli/bin/ng.js build
 cd ..
 
 rsync -rv spt-webui-frontend/dist/spt-webui-frontend/browser/* deploy@niklas:/var/www/spt-webui/
-rsync -rv spt_webui_backend pyproject.toml poetry.lock README.md alembic alembic.ini pyproject.toml deploy@niklas:/var/www/spt-webui-backend/
+rsync -rv spt_webui_backend pyproject.toml uv.lock README.md deploy@niklas:/var/www/spt-webui-backend/
